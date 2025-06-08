aab39f8d9b871ef6ed4955458ae881e7692690ac
madurangaPrabhath <maduracourse@gmail.com>
2025-06-08 14:25:36 +0530

    Add images to Testimonials component

--- frontend/src/components/Testimonials.js
+++ frontend/src/components/Testimonials.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../assets/styles/Testimonials.css';
-{/* import user1 from '../assets/images/user1.jpg';
-import user2 from '../assets/images/user2.jpg';
-import user3 from '../assets/images/user3.jpg';*/} 
+import user1 from '../assets/images/female.jpg';
+import user2 from '../assets/images/man1.jpg';
+import user3 from '../assets/images/man2.jpg';
 
 const testimonials = [
   {
@@ -10,7 +10,7 @@ const testimonials = [
       "As a contractor, I save time and money using this site. Everything’s in one place, and the service is top-notch.",
     name: 'R.K.S. Amilia',
     location: 'Gampaha',
-    //image: user1,
+    image: user1,
     cardStyle: 'white-card',
   },
   {
@@ -18,7 +18,7 @@ const testimonials = [
       'TopRoof Solutions made it so easy to find the roofing sheets I needed. Delivered on time and great quality!',
     name: 'T.S.D. Jane',
     location: 'Kandy',
-    //image: user2,
+    image: user2,
     cardStyle: 'blue-card',
   },
   {
@@ -26,7 +26,7 @@ const testimonials = [
       'Hard to find gutters like this anywhere else. Very happy with the purchase. Delivered on time and great quality!',
     name: 'R.K.O. Levis',
     location: 'Piliyandala',
-    //image: user3,
+    image: user3,
     cardStyle: 'blue-card',
   },
 ];
