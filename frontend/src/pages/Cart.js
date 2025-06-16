5e338ec266bd7e59066f8af26265e4f5017d073c
madurangaPrabhath <maduracourse@gmail.com>
2025-06-16 11:31:01 +0530

    Edit the Cart page with CSS styles

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -2,21 +2,28 @@ import React from 'react';
 import Header from '../components/Header';
 import NewsletterSection from '../components/NewsletterSection';
 import Footer from '../components/Footer';
+import { FaShoppingCart } from 'react-icons/fa';
+import '../assets/styles/Cart.css';
 
 const Cart = () => {
   return (
     <div>
       <Header />
-      <div style={{ padding: '40px' }}>
-        <h2>Your Shopping Cart</h2>
-        <p>No items in your cart yet.</p>
+      <div className="cart-page">
+        <h2 className="cart-title">Your Shopping Cart</h2>
+        <div className="empty-cart-container">
+          <div className="empty-cart-container">
+            <FaShoppingCart className="empty-cart-icon" />
+            <p className="empty-cart-text">Your cart is currently empty.</p>
+            <a href="/products" className="shop-now-button">Browse Products</a>
+          </div>
+        </div>
       </div>
       <NewsletterSection />
       <section id="contact">
         <Footer />
       </section>
     </div>
-
   );
 };
 
