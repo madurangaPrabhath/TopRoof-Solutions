40f0db1ea854c4d4059e0e665c7fa1637672d62a
madurangaPrabhath <maduracourse@gmail.com>
2025-06-20 16:51:33 +0530

    Edit WebConfig

--- backend/src/main/java/com/toproof/backend/config/WebConfig.java
+++ backend/src/main/java/com/toproof/backend/config/WebConfig.java
@@ -1,15 +1,17 @@
 package com.toproof.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
